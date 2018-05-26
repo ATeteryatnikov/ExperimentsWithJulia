1e2eb266e7ad38919fdc1bea7307d56d44593ef5
ATeteryatnikov <volk1695@mail.ru>
2018-05-26 09:08:38 +0700

    Реализован алгоритм поиска сингулярных чисел через метод бисекций. Добавлены тесты

--- SVD singular values/DevideAndConqueror.jl
+++ SVD singular values/DevideAndConqueror.jl
@@ -36,7 +36,7 @@ function FDACA(x, alpha, betta, lenBlock, s="", mode="T")
                 #X2 = det(matr - x*eye(len-k))
 
                 #print("X2: ", X2, "\n")
-
+                #return matr
                 #show(matr)
                 #print("\n")
                 #print("len alpha = ", length(alpha[k+1:end]), "\n")
@@ -117,6 +117,100 @@ function FDACA(x, alpha, betta, lenBlock, s="", mode="T")
         X0 = X1*X2 + abs(betta[k])*X1*X4 + abs(betta[k])*X2*X3
         #print("\n X0 is\n x1: ", X1, " *  x2 ", X2, " x3 ", X3, " x4 ", X4, " bettak ", betta[k], "\n" )
         #print("call : ",mode, s, " end \n")
+
+        #=sig = sign(X2)*sign(X3)*sign(X1)*sign(X4)
+        #print(sig, "\n")
+        psi = -log(abs(betta[k])) - log(abs(exp(X4-X2) + sig*exp(X3-X1)))
+        if (sign(psi)==sign(X0))
+            print("Совпали\n")
+        end=#
         return X0
     end
+end
+
+""" Поиск сингулярных чисел между двумя заданными числами """
+function bisection(x, y, prec, alpha, betta, lenBlock)
+    k = 0
+
+    x = big(x)
+    y = big(y)
+    z = big(0.0)
+
+    if (sign(FDACA(x, copy(alpha), copy(betta), lenBlock))==sign(FDACA(y, copy(alpha), copy(betta), lenBlock)))
+        return 0
+    end
+
+    while ( (y-x)>prec )
+        k = k + 1
+        h = (y-x)/2
+        z = x + h
+
+        #print("\nX: ", Float64(x),"Z: ", Float64(z),"Y: ", Float64(y))
+        fdacaZ = FDACA(z, copy(alpha), copy(betta), lenBlock)
+        znakZ = Int(sign(fdacaZ))
+        fdacaX = FDACA(x, copy(alpha), copy(betta), lenBlock)
+        znakX = Int(sign(fdacaX))
+        fdacaY = FDACA(y, copy(alpha), copy(betta), lenBlock)
+        znakY = Int(sign(fdacaY))
+        if (znakX == znakY)
+            print("\nОШИБКА ТУТ\n")
+            print("\nX: ", Float64(x)," Z: ", Float64(z)," Y: ", Float64(y))
+            print("FDACAx: ", fdacaX,"\n")
+            print("FDACAy: ", fdacaY,"\n")
+            #print("FDACAz \n", fdacaZ,"\n")
+            throw("знаки совпали X=Y (1)")
+        end
+        #print(znak)
+        #print("\n znakX, znakZ, znakY: ", znakX, " " , znakZ, " ", znakY)
+        if (znakZ == znakX)
+            x = z
+        #    print(" X == Z")
+        elseif (znakZ == znakY)
+            y = z
+            #print(" Y == Z")
+        end
+
+    end
+    #print(k, "\n")
+    return z
+end
+
+""" поиск сингулярных чисел между сингулярными числами дочерных матриц (Т0 и Т1) """
+function svdValsFinder(alpha, betta, lenBlock, prec)
+
+    len = length(alpha)
+
+    if (len <= lenBlock)
+
+        matr = toDense(alpha, betta)
+        return svdvals(matr)
+    else
+        k = div(len, 2)
+
+        alpha[k] = alpha[k] - abs(betta[k])
+        alpha[k+1] = alpha[k+1] - abs(betta[k])
+
+        if (k-1 >= 0)
+            singValsT0 = svdValsFinder(copy(alpha[1:k]), copy(betta[1:k-1]), lenBlock, prec)
+        end
+        if (len-(k+1) >= 0)
+            singValsT1 = svdValsFinder(copy(alpha[k+1:end]), copy(betta[k+1:end]), lenBlock, prec)
+
+        end
+
+        unionSingVals = sort(union(singValsT0, singValsT1), rev=true)
+        #return unionSingVals
+        singVals = big.(zeros(len))
+        #return unionSingVals
+        for i in 1:1:len-1
+            singVals[i] = bisection(unionSingVals[i+1], unionSingVals[i], prec, copy(alpha), copy(betta), lenBlock)
+            print(string(i,": ",Float64(singVals[i]),"\n"))
+        end
+        singVals[len] = bisection(unionSingVals[len], unionSingVals[len]+2*betta[k], prec, copy(alpha), copy(betta), lenBlock)
+        print(string(len,": ",Float64(singVals[len]),"\n"))
+        return singVals
+
+    end
+
+
 end
--- SVD singular values/DevideAndConquerorTests.jl
+++ SVD singular values/DevideAndConquerorTests.jl
@@ -2,123 +2,133 @@ try
     include("src/Matrices.jl")
     include("Lanczos.jl")
     include("DevideAndConqueror.jl")
+    using GenericSVD
 catch ex
     print("Что-то пошло не так: ", ex)
 end
 
-setprecision(200)
-function bisection(x, y, alpha, betta, lenBlock)
-    k = 0
-    x = big(x)
-    y = big(y)
-    z = big(0.0)
-    if (sign(FDACA(x, copy(alpha), copy(betta), lenBlock))==sign(FDACA(y, copy(alpha), copy(betta), lenBlock)))
-        return "Знаки совпали"
-    end
+numTest = 5
 
-    while ( (y-x)>big(1e-10) )
-        k = k + 1
-        h = (y-x)/2
-        z = x + h
+# 1)
+###########################  Check FDACA Bisection test
+if (numTest == 1)
+    print("\nТест 1) Ищем сингулярное число матрицы 6х6 методом бисекций и ",
+         "\nнаходим значение характеристического многочлена в этой точке")
+    setprecision(300)
+    lenBlock = 4
 
-        #print("\nX: ", Float64(x),"Z: ", Float64(z),"Y: ", Float64(y))
-        fdacaZ = FDACA(z, copy(alpha), copy(betta), lenBlock)
-        znakZ = Int(sign(fdacaZ))
-        fdacaX = FDACA(x, copy(alpha), copy(betta), lenBlock)
-        znakX = Int(sign(fdacaX))
-        fdacaY = FDACA(y, copy(alpha), copy(betta), lenBlock)
-        znakY = Int(sign(fdacaY))
-        if (znakX == znakY)
-            print("\nОШИБКА ТУТ\n")
-            print("\nX: ", Float64(x)," Z: ", Float64(z)," Y: ", Float64(y))
-            print("FDACAx: ", fdacaX,"\n")
-            print("FDACAy: ", fdacaY,"\n")
-            #print("FDACAz \n", fdacaZ,"\n")
-            throw("знаки совпали X=Y (1)")
-        end
-        #print(znak)
-        #print("\n znakX, znakZ, znakY: ", znakX, " " , znakZ, " ", znakY)
-        if (znakZ == znakX)
-            x = z
-        #    print(" X == Z")
-        elseif (znakZ == znakY)
-            y = z
-            #print(" Y == Z")
-        end
+    alphas = [1 3 0.000007 9999 5 8]
+    bettas = [2 500 9 1 7]
+    Af = toDense(alphas, bettas)
 
-    end
-    #print(k, "\n")
-    return z
+    eigVal = svdvals(Af)[2]
+    #print("\nOne eigen value is: ", eigVal)
+
+    detInOneEigVal = det(Af - eigVal*eye(6))
+    #print("\n determinant in one eigen value point: ", detInOneEigVal)
+    detFDACA = FDACA(eigVal, copy(alphas), copy(bettas), 4)
+    #print("\n determinant by FDACA in one eigen value point: ", detFDACA)
+    print("\n Разность значений  характеристических многочленов",
+          "\n полученных разными способами в точке сингулярного числа: ", detFDACA-detInOneEigVal)
+
+    OneEigenValue = bisection(-1,0.5, 1e-20, copy(alphas), bettas, lenBlock)
+    print("\n метод бисекций получает сингулярное число: ", OneEigenValue)
+    fdacaRes = FDACA(OneEigenValue, copy(alphas), bettas, lenBlock)
+    print("\n Характеристический многочлен принимает значение: ", fdacaRes)
 end
 
-###########################  Check FDACA with Bisection test
-#=
-x = 1
-lenBlock = 4
-
-alphas = [1 3 0.000007 9999 5 8]
-bettas = [2 500 9 1 7]
-Af = toDense(alphas, bettas)
-res1 = det(Af - x*eye(6))
-res2 = FDACA(x, copy(alphas), copy(bettas), 4)
-diffres = res1 - res2
-
-eigVal = eigvals(Af)[2]
-print("\nOne eigen value is: ", eigVal)
-
-d = det(Af - eigVal*eye(6))
-print("\n determinant in one eigen value point: ", d)
-detFDACA = FDACA(eigVal, copy(alphas), copy(bettas), 4)
-print("\n determinant by FDACA in one eigen value point: ", detFDACA)
-
-oneValue = bisection(-1,0.5, alphas, bettas, lenBlock)
-print("\n метод бисекций получает: ",oneValue)
-fdacaRes = FDACA(oneValue, copy(alphas), bettas, lenBlock)
-print("\n Определитель в точке = ", fdacaRes)=#
-
-###########################  Check FDACA
-
-#=
-bigFloatMantissa = 100
-n = 100
-x = sqrt(656.6815092957207)
-lenBlock = 4
-
-setprecision(bigFloatMantissa)
-A = getGodunovMatrix(n)[1]
-
-listsAlphAndBetta = lanczos(A)
-denseTridiagonalMatrix = toDense(listsAlphAndBetta[1], listsAlphAndBetta[2])
-
-detTridiagonalMatrix = det(denseTridiagonalMatrix - x*eye(n))
-detByFDACA = FDACA(x, listsAlphAndBetta[1], listsAlphAndBetta[2], lenBlock)
-
-diffDet = detTridiagonalMatrix - detByFDACA
-=#
-
+# 2)
 ############    Check on Godunov matrix
-x = -1
-lenBlock = 4
-n = 10
+if (numTest == 2)
+    print("\nТест 2) Ищем сингулярное число матрицы Годунова методом бисекций и ",
+         "\nнаходим значение характеристического многочлена в этой точке")
+    setprecision(500)
 
-godunovLists = lanczos(getGodunovMatrix(n)[1])
-alphas = godunovLists[1]
-bettas = godunovLists[2]
+    x = -1
+    lenBlock = 4
+    n = 10
 
-Af = toDense(alphas, bettas)
-res1 = det(Af - x*eye(n))
-res2 = FDACA(x, copy(alphas), copy(bettas), 4)
-diffres = res1 - res2
+    godunovLists = lanczos(getGodunovMatrix(n)[1])
+    alphas = godunovLists[1]
+    bettas = godunovLists[2]
 
-eigVal = svdvals(Af)[2]
-print("\nOne eigen value is: ", eigVal)
 
-d = det(Af - eigVal*eye(n))
-print("\n determinant in one eigen value point: ", d)
-detFDACA = FDACA(eigVal, copy(alphas), copy(bettas), 4)
-print("\n determinant by FDACA in one eigen value point: ", detFDACA)
+    eigVal = svdvals(Af)[2]
 
-oneValue = bisection(5.5,5.8, copy(alphas), bettas, lenBlock)
-print("\n метод бисекций получает: ",oneValue)
-fdacaRes = FDACA(oneValue, copy(alphas), bettas, lenBlock)
-print("\n Определитель в точке = ", fdacaRes)
+    detInOneEigVal = det(Af - eigVal*eye(6))
+    #print("\n determinant in one eigen value point: ", detInOneEigVal)
+    detFDACA = FDACA(eigVal, copy(alphas), copy(bettas), 4)
+    #print("\n determinant by FDACA in one eigen value point: ", detFDACA)
+    print("\n Разность значений  характеристических многочленов",
+          "\n полученных разными способами в точке сингулярного числа: ", detFDACA-detInOneEigVal)
+
+    OneEigenValue = bisection(-1,0.5, big(1e-20), copy(alphas), bettas, lenBlock)
+    print("\n метод бисекций получает сингулярное число: ", OneEigenValue)
+    fdacaRes = FDACA(OneEigenValue, copy(alphas), bettas, lenBlock)
+    print("\n Характеристический многочлен принимает значение: ", fdacaRes)
+end
+
+# 3)
+###########################
+if (numTest == 3)
+    print("\nТест 3) Ищем значение характеристического многочлена в произвольной точке ",
+         "\nдля трехдиагональной матрицы полученной из матрицы матрицы Годунова методом Ланцоша")
+    setprecision(100)
+    x = 20
+
+    bigFloatMantissa = 100
+    n = 10
+    lenBlock = 4
+
+    setprecision(bigFloatMantissa)
+    A = getGodunovMatrix(n)[1]
+
+    listsAlphAndBetta = lanczos(A)
+    denseTridiagonalMatrix = toDense(listsAlphAndBetta[1], listsAlphAndBetta[2])
+
+    detTridiagonalMatrix = det(denseTridiagonalMatrix - x*eye(n))
+    detByFDACA = FDACA(x, listsAlphAndBetta[1], listsAlphAndBetta[2], lenBlock)
+
+    diffDet = detTridiagonalMatrix - detByFDACA
+    print("\n Разность значений характеристических многочленов в точке: ", diffDet)
+end
+
+# 4)
+####### test FDACA signum
+if (numTest == 4)
+    print("\nТест 4) Ищем разными методами значение характеристического многочлена для матрицы 6х6",
+         "\nв сингулярных точках матрицы полученных методом svdvals. Сравниваем знак")
+    setprecision(100)
+
+    lenBlock = 4
+
+    alphas = [1 3 0.000007 9999 5 8]
+    bettas = [2 500 9 1 7]
+    Af = toDense(alphas, bettas)
+    singOrigin = svdvals(Af)
+
+    for i in 1:1:6
+        detOrigin = det(Af - singOrigin[i]*eye(6))
+        #print(Float64(detOrigin), "\n")
+        detFDACA = FDACA(singOrigin[i], copy(alphas), copy(bettas), 4)
+        #print(Float64(detFDACA), "\n")
+
+        if sign(detOrigin)!=sign(detFDACA)
+            print("Проблема на сингулярном числе № ",i,"\n")
+        end
+    end
+end
+
+#########   Check svdValsFinder
+if (numTest == 5)
+
+    lenBlock = 4
+
+    alphas = [1 3 0.000007 9999 5 8]
+    bettas = [2 500 9 1 7]
+    Af = toDense(alphas, bettas)
+
+    sing = svdValsFinder(copy(alphas), copy(bettas), lenBlock, 1e-20)
+    singOrigin = svdvals(Af)
+
+end
